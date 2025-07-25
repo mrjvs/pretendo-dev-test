e12a3d580ea813b6d44da0100d28ab880eb79756
mrjvs <jellevs@gmail.com>
2025-07-25 16:57:38 +0200

    Make mitmproxy use the pregenerated cert

--- assets/certs/generate.sh
+++ assets/certs/generate.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname "$0")"
 
 echo "-- GENERATING CERTIFICATES --"
-openssl genrsa -out ca.key 2048
+openssl genrsa -out ca.key 1024
 openssl req -new -x509 -key ca.key -out ca.pem -subj "/C=US/ST=Florida/O=Pretendo"
 cat ca.key ca.pem > mitm-ca.pem
 echo "Done!"
